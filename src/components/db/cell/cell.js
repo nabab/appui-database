84ae5ab8ad45db58833cb2c2b5e9e9ed8e5359df
dev-clovis.bbn.io <dev-clovis@qr.vm.m3l.co>
2023-04-19 06:23:50 -0400

    missing semicolons

--- src/components/db/cell/cell.js
+++ src/components/db/cell/cell.js
@@ -6,7 +6,7 @@
       let link = host.root + 'tabs/' + host.source.engine + '/' + host.source.info.code + '/' + this.source.name + '/home';
       return {
         link: link
-      }
+      };
     }
-  }
+  };
 })();
