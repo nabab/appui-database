a2a06027e0ab46b464bc3dbe956cc917b01aa453
dev-clovis <clovis@bbn.io>
2023-06-02 05:18:12 -0400

    valid form conditional data search

--- src/mvc/model/actions/column/validform.php
+++ src/mvc/model/actions/column/validform.php
@@ -11,13 +11,15 @@ use bbn\X;
 $res = ['success' => false];
 
 if ($model->hasData(['engine', 'db', 'host', 'table'])) {
-	/*$database = new bbn\Appui\Database($model->db);
+  $database = new bbn\Appui\Database($model->db);
   $conn = $database->connection($model->data['host'], $model->data['engine'], $model->data['db']);
-  $data = $conn->getColumnValues($model->data['table'], $model->data['name']);*/
-  //X::ddump($conn, $data, $model->data['table'], $model->data['name'], $model->data['host'], $model->data['engine'], $model->data['db'], $model->data);
+  $data = [];
+  if ($model->haData('name')) {
+    $data = $conn->getColumnValues($model->data['table'], $model->data['name']);
+  }
   $res = [
     'success' => true,
-    'num' => 0
+    'num' => count($data);
   ];
 }
 
