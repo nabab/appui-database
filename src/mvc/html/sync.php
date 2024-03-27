6e079af7bcb8a330028f389da4b9444c9c1ee0f7
nabab <thomas.nabet@gmail.com>
2024-03-27 02:50:39 +0100

    bbn-cp

--- src/mvc/html/sync.php
+++ src/mvc/html/sync.php
@@ -2,21 +2,21 @@
             :nav="true">
   <bbns-container title="<?= _('Errors') ?>"
                   url="errors"
-                  :static="true"
+                  :fixed="true"
                   icon="nf nf-pom-internal_interruption"
                   :source="source"
                   component="appui-database-sync-errors">
   </bbns-container>
   <bbns-container title="<?= _('Conflicts') ?>"
                   url="conflicts"
-                  :static="true"
+                  :fixed="true"
                   icon="nf nf-mdi-vector_difference"
                   :source="source"
                   component="appui-database-sync-conflicts">
   </bbns-container>
   <bbns-container title="<?= _('Structures') ?>"
                   url="structures"
-                  :static="true"
+                  :fixed="true"
                   icon="nf nf-mdi-table_settings"
                   :source="source"
                   component="appui-database-sync-structures">
