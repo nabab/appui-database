d787e6696ad408bed7985d9fb167d816d2fd46ac
nabab <thomas.nabet@gmail.com>
2024-12-22 17:16:15 +0100

    Fix in host

--- src/mvc/model/tabs/host.php
+++ src/mvc/model/tabs/host.php
@@ -11,19 +11,29 @@ if ( $host_id = $model->inc->dbc->hostId($model->data['host']) ){
   $dbs = $model->inc->dbc->fullDbs($host_id);
 
   try {
+    /** @var bbn\Db $conn */
     $conn = $model->inc->dbc->connection($host_id, $model->data['engine']);
   }
-  catch (\Exception $e) {
+  catch (Exception $e) {
     return ['error' => $e->getMessage()];
   }
+
   if (empty($o['databases'])) {
     $dbs = $model->getModel($model->pluginUrl('appui-database').'/data/dbs', [
       'host_id' => $host_id
     ]);
   }
+
+  try {
+    $size = $conn ? $conn->dbSize($model->data['db'] ?? $conn->getCurrent()) : 0;
+  }
+  catch (Exception $e) {
+    $size = 0;
+  }
+
   return [
     'success' => true,
-    'size' => $isReal ? bbn\Str::saySize($model->db->dbSize($model->data['db'])) : 0,
+    'size' => $size,
     'types' => bbn\Db\Languages\Sql::$types,
     'predefined' => $model->inc->options->fullOptions('pcolumns', $model->data['engine'], 'database', 'appui'),
     'dbs' => $dbs,
